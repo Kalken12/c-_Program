fe970576096eeb7a2f6aecf23fc5e0ae63e8d54c
Kalken12 <82832370+Kalken12@users.noreply.github.com>
2021-05-08 22:45:55 +0530

    Given no of days convert into year-week-days

---
+++ C_GivenDaysConvertYearWeekDays.c
@@ -0,0 +1,20 @@
+//C program to convert specified days into years, weeks and days. Note- ignore leap year.
+
+#include <stdio.h>
+int main()
+{
+	int year, days, week;
+	int no_days;
+	printf("Enter number off days");
+	scanf("%d",&no_days);
+	
+	year = no_days/365;
+	week = (no_days % 365) /7;
+	days = no_days - ((year*365) + (week*7));
+	
+	printf("%d\n",year);
+	printf("%d\n",week);
+	printf("%d\n",days);
+	return 0;
+		
+}
