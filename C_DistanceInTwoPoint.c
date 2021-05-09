190ac400eaa7f2d375030fff4a558333ed030fb1
Kalken12 <82832370+Kalken12@users.noreply.github.com>
2021-05-09 19:38:35 +0530

    Program to calculate distance between two points

---
+++ C_DistanceInTwoPoint.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	float x1, y1, x2, y2, gdistance;
+	printf("Input x1: ");
+	scanf("%f", &x1);
+	printf("Input y1: ");
+	scanf("%f", &y1);
+              printf("Input x2: ");
+	scanf("%f", &x2);
+	printf("Input y2: ");
+	scanf("%f", &y2);
+	gdistance = ((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1));
+	printf("Distance between the said points: %.4f", sqrt(gdistance));
+	printf("\n");
+	return 0;
+}
