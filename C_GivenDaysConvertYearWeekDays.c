784346e2563887561de5b28196681842b4cedb11
Kalken12 <82832370+Kalken12@users.noreply.github.com>
2021-05-09 18:21:39 +0530

    Given days convert into year, month, week and days.

--- C_GivenDaysConvertYearWeekDays.c
+++ C_GivenDaysConvertYearWeekDays.c
@@ -3,18 +3,21 @@
 #include <stdio.h>
 int main()
 {
-	int year, days, week;
+	int year, days, week, month;
 	int no_days;
 	printf("Enter number off days");
 	scanf("%d",&no_days);
 	
 	year = no_days/365;
+	month = (no_days %365)/30;
 	week = (no_days % 365) /7;
 	days = no_days - ((year*365) + (week*7));
 	
 	printf("%d\n",year);
+	printf("%d\n", month);
 	printf("%d\n",week);
 	printf("%d\n",days);
+
 	return 0;
 		
 }
