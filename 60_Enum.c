e5478040729a44a11305a4df935adccf7d625b8f
Kalken12 <82832370+Kalken12@users.noreply.github.com>
2021-05-16 18:50:32 +0530

    Program using 'Enum' user defined data type.

---
+++ 60_Enum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+enum week{ Mon,Sun, Tue, Wed, Thu, Fri, Sat};
+printf("Sun = %d", Sun);
+printf("\nMon = %d", Mon);
+printf("\nTue = %d", Tue);
+printf("\nWed = %d", Wed);
+printf("\nThu = %d", Thu);
+printf("\nFri = %d", Fri);
+printf("\nSat = %d", Sat);
+
+return 0;
+}
